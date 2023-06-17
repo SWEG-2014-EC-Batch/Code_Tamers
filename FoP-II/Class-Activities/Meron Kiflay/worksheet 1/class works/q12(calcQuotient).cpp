efe456238c0650076a3b4d5067eef6f5f458bd79
Meron Kiflay <129954153+me-ron@users.noreply.github.com>
2023-06-17 22:34:09 +0300

    Add files via upload

---
+++ FoP-II/Class-Activities/Meron Kiflay/worksheet 1/class works/q12(calcQuotient).cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+void calcQuotient(double n1, double n2, double& answer);
+
+int main() {
+    double num1, num2, quotient;
+
+    cout << "Enter the first number: ";
+    cin >> num1;
+
+    cout << "Enter the second number: ";
+    cin >> num2;
+
+    calcQuotient(num1, num2, quotient);
+
+    cout << "Quotient: " << quotient << endl;
+
+    return 0;
+}
+
+void calcQuotient(double n1, double n2, double& answer) {
+    if (n2 != 0) {
+        answer = n1 / n2;
+    } else {
+        cout << " Division by zero ain't allowed." << endl;
+        answer = 0;
+    }
+}

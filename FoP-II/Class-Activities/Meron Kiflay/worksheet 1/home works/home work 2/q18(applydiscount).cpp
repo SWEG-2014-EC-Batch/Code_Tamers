b81dbd86be0eaaaa68641aaa2917795a44cad8d4
Meron Kiflay <129954153+me-ron@users.noreply.github.com>
2023-06-18 22:08:35 +0300

    Create q18(applydiscount).cpp

---
+++ FoP-II/Class-Activities/Meron Kiflay/worksheet 1/home works/home work 2/q18(applydiscount).cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<cassert>
+using namespace std;
+void applyDiscount(double& price, double discount , bool isPercentage ){
+   assert(discount>=0);
+   if(isPercentage){
+        price = price * (1 - discount/100);
+   }
+   else{
+        price -= discount;
+   }
+   assert(price!=0);
+}
+int main(){
+    double price, discount;
+    bool isPercentage = true;
+    char choice;
+    cout<<"Enter the price of the item: "<<endl;
+    cin>>price;
+    cout<<"Enter the discount amount or percent: "<<endl;
+    cin>>discount;
+    cout<<"Is the discount fixed or percentage: "<<endl;
+    cout<<"\"f\" for fixed and \"p\" for percentage: "<<endl;
+    cin>>choice;
+    if(choice == 'f'){
+        isPercentage = false;
+    }
+    cout<<"The original price is "<<price<<" $"<<endl;
+    applyDiscount(price, discount, isPercentage);
+    cout<<endl<<"The price after discount is "<<price<<" $"<<endl;
+return 0;
+}

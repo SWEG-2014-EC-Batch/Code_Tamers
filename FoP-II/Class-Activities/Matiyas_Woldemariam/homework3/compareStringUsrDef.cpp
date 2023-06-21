26f8ac055df421cdc7a210fbd8e05a2e00bee979
matiyasjosh <119722911+matiyasjosh@users.noreply.github.com>
2023-06-21 21:19:52 +0300

    header file activities

--- FoP-II/Class-Activities/Matiyas_Woldemariam/homework3/compareStringUsrDef.cpp
+++ FoP-II/Class-Activities/Matiyas_Woldemariam/homework3/compareStringUsrDef.cpp
@@ -3,24 +3,10 @@
 #include "matiString.h"
 using namespace std;
 
-int compare(string say1, string say2);
-
 int main() {
     string msg1, msg2;
     cout<<"Enter two messages to be compared: \n";
     getline(cin, msg1);
     getline(cin, msg2);
     cout<<"When comparing the two strings: "<<compare(msg1, msg2);
-}
-
-int compare(string say1, string say2) {
-    int x, y;
-    x = len(say1);
-    y = len(say2);
-    if(x==y) 
-        return 0;
-    else if(x>y)
-        return 1;
-    else    
-        return -1;
 }

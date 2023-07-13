a7ed88de182ac7adf5fd38ac9b05dc29417352a0
endragnee1 <anime.game7415@protonmail.com>
2023-07-13 22:15:27 +0300

    Rebasing the input function for interst program

---
+++ FoP-II/Project-work/V1/IntersetCacl/interseT.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<iomanip>
+
+using namespace std;
+
+float inteRate,overInterst=0,paymenT,interseT,loaN,originaLoan,annualRate;
+int i=0;
+
+void inpuT();
+
+int main()
+{
+    inpuT();
+}
+
+void inpuT()
+{
+    cout<<"Enter the the original loan:"<<endl<<">";
+    cin>>loaN;
+    originaLoan=loaN;
+    cout<<"Enter the interst rate of the loan(the rate should be in decimal):"<<endl<<">";
+    cin>>inteRate;
+    system("clear");
+    cout<<setw(20)<<"Loan(in Birr)"<<setw(20)<<"Payment(in Birr)"<<setw(20)<<"Interst(in Birr)"<<setw(20)<<"Annual Rate(in %)"<<endl<<endl;
+}

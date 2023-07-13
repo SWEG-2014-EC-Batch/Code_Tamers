06b0e82cfea01bdc6431d17f5bc81608479f493e
meron weldu <meron.w.teka@gmail.com>
2023-07-13 16:15:04 +0200

    Update phone 1.cpp

--- FoP-II/Class-Activities/Meron Weldu/homeworks/homework UDDT/phone 1.cpp
+++ FoP-II/Class-Activities/Meron Weldu/homeworks/homework UDDT/phone 1.cpp
@@ -9,18 +9,21 @@ struct phone {
 };
 
 int main() {
+   
+    phone phoneNumber1 = {212, 767, 8900};
 
-    phone phoneNumber1;
+    
+    phone phoneNumber2;
     cout << "Enter an area code: ";
-    cin >> phoneNumber1.areaCode;
+    cin >> phoneNumber2.areaCode;
     cout << "Enter an exchange: ";
-    cin >> phoneNumber1.exchange;
+    cin >> phoneNumber2.exchange;
     cout << "Enter a number: ";
-    cin >> phoneNumber1.number;
+    cin >> phoneNumber2.number;
 
-    cout << "Phone number : (" << phoneNumber1.areaCode << ") " << phoneNumber1.exchange << "-" << phoneNumber1.number << endl;
-
+   
+    cout << "Phone number 1: (" << phoneNumber1.areaCode << ") " << phoneNumber1.exchange << "-" << phoneNumber1.number << endl;
+    cout << "Phone number 2: (" << phoneNumber2.areaCode << ") " << phoneNumber2.exchange << "-" << phoneNumber2.number << endl;
 
     return 0;
 }
-

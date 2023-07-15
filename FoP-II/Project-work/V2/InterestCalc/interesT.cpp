0cee2ae550f4b55f45359f59928d13a4fead8ea6
matiyasjosh <119722911+matiyasjosh@users.noreply.github.com>
2023-07-15 23:16:47 +0300

    updating ...

--- FoP-II/Project-work/V2/InterestCalc/interesT.cpp
+++ FoP-II/Project-work/V2/InterestCalc/interesT.cpp
@@ -8,17 +8,32 @@ float inteRate,overInterst=0,paymenT,interseT,loaN,originaLoan,annualRate;
 int i=0;
 }it;
 
-
-
+void inpuT();
+void dataCalc();
 void outpuT();
 
+int main()
+{
+    inpuT();
+    dataCalc();
+    outpuT();
+}
 
+void inpuT()
+{
+    cout<<"Enter the the original loan:"<<endl<<">";
+    cin>>it.loaN;
+    it.originaLoan=it.loaN;
+    cout<<"Enter the interst rate of the loan(the rate should be in decimal):"<<endl<<">";
+    cin>>it.inteRate;
+    system("clear");
+    cout<<setw(20)<<"Loan(in Birr)"<<setw(20)<<"Payment(in Birr)"<<setw(20)<<"Interst(in Birr)"<<setw(20)<<"Annual Rate(in %)"<<endl<<endl;
+}
 
 void dataCalc()
 {
-
-
-    
+    it.paymenT=it.originaLoan/20;
+    it.interseT=(it.inteRate*it.loaN)/12;      
     it.annualRate=(it.loaN/it.originaLoan)*100;
     it.overInterst=it.paymenT-it.interseT;
     it.loaN-=it.overInterst;

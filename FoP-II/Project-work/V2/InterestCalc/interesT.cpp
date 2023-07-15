e47c96aa921d01446eecbdcbc12500ad8082ffd4
meron weldu <meron.w.teka@gmail.com>
2023-07-15 20:28:38 +0200

    Update interesT.cpp

--- FoP-II/Project-work/V2/InterestCalc/interesT.cpp
+++ FoP-II/Project-work/V2/InterestCalc/interesT.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+using namespace std;
 
+struct pattern {
+    int row , initial1=0, inital2=1;
+};
+
+
+
+char lower(int n, int i);
+
+
+
+
+
+
+
+
+char lower(int n, int i) {
+    int space=0, number=i;
+    if(i==1)
+        n--;
+    while(space<n) {
+        cout<<"  ";
+        space++;
+    }

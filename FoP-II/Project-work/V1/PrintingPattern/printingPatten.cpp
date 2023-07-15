3deceead43f8c0bf5026e2e6a12971c2766459ce
meron weldu <meron.w.teka@gmail.com>
2023-07-15 13:31:18 +0200

    Update printingPatten.cpp

--- FoP-II/Project-work/V1/PrintingPattern/printingPatten.cpp
+++ FoP-II/Project-work/V1/PrintingPattern/printingPatten.cpp
@@ -3,3 +3,20 @@
 using namespace std;
 
 int num;
+
+
+
+
+
+
+
+void lower_part()
+{
+    for (int n = 1; n <= num; n++)
+    {
+        for (int f = num - n; f > 0; f--)
+            cout << " ";
+        for (int z = n; z >= 1; z--)
+            cout << z;
+
+

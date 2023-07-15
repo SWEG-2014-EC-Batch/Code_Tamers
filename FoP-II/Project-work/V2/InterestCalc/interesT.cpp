5293a1aff0e7841b2170151ad82a2c18822d4d7c
meron weldu <meron.w.teka@gmail.com>
2023-07-15 21:22:30 +0200

    Update interesT.cpp

--- FoP-II/Project-work/V2/InterestCalc/interesT.cpp
+++ FoP-II/Project-work/V2/InterestCalc/interesT.cpp
@@ -1,1 +1,35 @@
+#include<iostream>
+#include<iomanip>
 
+using namespace std;
+
+struct interest {
+float inteRate,overInterst=0,paymenT,interseT,loaN,originaLoan,annualRate;
+int i=0;
+}it;
+
+
+
+
+void outpuT();
+
+
+
+
+
+
+
+
+
+
+
+void outpuT()
+{
+    dataCalc();
+    if(it.loaN>=0)
+    {
+        it.i++;
+        cout<<setw(20)<<it.loaN<<setw(20)<<it.paymenT<<setw(20)<<it.interseT<<setw(20)<<it.annualRate<<"%"<<endl;
+        
+        outpuT();
+    }

a8789407828d4d6b37f149a7736bcbd6e774671b
Makda-Yoseph <124851414+Makda-Yoseph@users.noreply.github.com>
2023-07-15 23:16:44 +0300

    Update printingPattern.cpp

--- FoP-II/Project-work/V2/PrintingPattern/printingPattern.cpp
+++ FoP-II/Project-work/V2/PrintingPattern/printingPattern.cpp
@@ -7,7 +7,24 @@ struct pattern {
 
 
 
-char lower(int n, int i);
+char upper(int n, int i) {
+    int space=n-i, number=n;
+    
+    while(space<n) {
+        cout<<"  ";
+        space++;
+    }
+    while(number>=0) {
+        cout<<number<<" ";
+        if (number==0) {
+            int l=1;
+            while(l<=n) {
+                cout<<l<<" ";
+                l++;
+            }
+        }
+        number--;
+    }
 
 
 

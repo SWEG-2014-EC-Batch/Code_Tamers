506e7e48740fabe0b1f15f02f67583297f7400a2
meron weldu <meron.w.teka@gmail.com>
2023-07-18 21:01:30 +0200

    Update Q1.cpp

--- FoP-II/Class-Activities/Meron Weldu/homeworks/homework file management/Q1.cpp
+++ FoP-II/Class-Activities/Meron Weldu/homeworks/homework file management/Q1.cpp
@@ -1,25 +1,25 @@
-#include <iostream>
-#include <fstream>
+#include<iostream>
+#include<fstream>
 using namespace std;
 
-// A function that takes the name of a file that contains integer records, an array of int and the address of a variable count
-void read_file(char* filename, int* array, int* count) {
-  // Open the file for reading
-  ifstream fin(filename);
-  // Check if the file is opened successfully
-  if (fin) {
-    // Initialize the count to zero
-    *count = 0;
-    // Read the integers from the file and store them in the array
-    while (fin >> array[*count]) {
-      // Increment the count
-      (*count)++;
+void readfile(const char* file,int arr[],int* count ){
+    ifstream f(file);
+    if(f){
+        *count = 0;
+        while (f >> arr[*count]){
+            (*count)++;
+        }
+        f.close();
     }
-    // Close the file
-    fin.close();
-  }
-  else {
-    // Print an error message
-    cerr << "Error: Cannot open file " << filename << endl;
-  }
+}
+
+int main(){
+    const char* file = "myfile.txt";
+    int arr[100];
+    int count;
+    readfile(file, arr, &count);
+    for(int i=0;i<count;i++){
+        cout<<arr[i]<<endl;
+    }
+    return 0;
 }

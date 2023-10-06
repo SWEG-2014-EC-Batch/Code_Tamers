4fe027f7544a97558daee48db19272614e9f4a97
matiyasjosh <119722911+matiyasjosh@users.noreply.github.com>
2023-10-06 18:42:26 +0300

    category rating function code

--- FoP-II/Project-work/Super-Market/header.h
+++ FoP-II/Project-work/Super-Market/header.h
@@ -34,3 +34,61 @@ typedef unordered_map<string, unordered_map<string, ItemInfo>> store;
 unordered_map<string, double> calculateCategoryRating();
 unordered_map<string, double> calculateItemRating();
 vector<pair<string, double>> rankCategories(const unordered_map<string, double>& categoryRatings);
+
+
+unordered_map<string, double> calculateCategoryRating() {
+    unordered_map<string, double> categoryRatings;
+
+    for(auto& categoryPair:vecStore[month])
+    {
+        const auto& category= categoryPair.first;
+        const auto& items= categoryPair.second;
+        
+        double categoryTotalRating = 0.0;
+        int itemCount = 0;
+
+        //iterating over each item in the category
+        for(auto& itemPair:items)
+        {
+            const auto& itemName = itemPair.first;
+            const auto& itemInfo = itemPair.second;
+
+            //calculating the rating for the item
+            double individualItemRating = itemInfo.numSold*100/itemInfo.numPurchased;
+
+            //adding it to categoryRating and increamenting the itemCount
+            categoryTotalRating += individualItemRating;
+            itemCount++;            
+        }
+
+        //then we need the average item rating to know the category rating
+        double averageCategoryRating = categoryTotalRating/itemCount;
+        
+        //variable represent the category rating    
+        double categoryRating;
+
+        if (averageCategoryRating>=90 && averageCategoryRating<=100)
+                categoryRating=5;
+        else if(averageCategoryRating>=80 && averageCategoryRating<90)
+            categoryRating=4.5;
+        else if(averageCategoryRating>=70 && averageCategoryRating<80)
+            categoryRating=4;
+        else if(averageCategoryRating>=60 && averageCategoryRating<70)
+            categoryRating=3.5;
+        else if(averageCategoryRating>=50 && averageCategoryRating<60)
+            categoryRating=3;
+        else if(averageCategoryRating>=40 && averageCategoryRating<50)
+            categoryRating=2.5;
+        else if(averageCategoryRating>=30 && averageCategoryRating<40)
+            categoryRating=2;
+        else if(averageCategoryRating>0 && averageCategoryRating<30)
+            categoryRating=1;
+        else   
+            cout<<"Error! There something wrong with the numbers in your data!"<<endl;
+
+        //store the average rating of the current category
+        categoryRatings[category] = categoryRating;
+    }
+
+    return categoryRatings;
+}

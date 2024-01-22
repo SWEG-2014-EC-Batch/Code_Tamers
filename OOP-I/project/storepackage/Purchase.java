9f13744a615332ce56815cf90b95a89fbf2e5f0e
meron weldu <meron.w.teka@gmail.com>
2024-01-22 14:59:24 +0100

    Update Purchase.java

--- OOP-I/project/storepackage/Purchase.java
+++ OOP-I/project/storepackage/Purchase.java
@@ -5,20 +5,14 @@ import project.user.Manager;
 // Purchase class
 public class Purchase extends Transaction {
     // additional attributes
-    private Manager manager;
     private Product product;
     
-    // constructor
-    public Purchase(int id, String date, double amount, Manager manager, Product product) {
-        super(id, date, amount); // call the superclass constructor
-        this.manager = manager;
-        this.product = product;
-    }
     
-    // getter methods
-    public Manager getManager() {
-        return manager;
-    }
+    // constructor
+    public Purchase(Product product) {
+        this.product = product;
+    
+}
     
     public Product getProduct() {
         return product;
@@ -28,18 +22,18 @@ public class Purchase extends Transaction {
     @Override
     public double calculate_price() {
         // calculate the cost of buying the product
-        return product.getPrice() * product.getQuantity();
+        return product.getPrice() * product.getAmount();
     }
     
     @Override
     public void payment() {
         // process the payment to the supplier
-        System.out.println("Payment made to " + product.getSupplier()); //this error is because of no getSupplier 
+        System.out.println("Payment made to " + product.getSupplier());
     }
     
     @Override
     public void update_stock() {
         // increase the stock of the product
-        product.setQuantity(product.getQuantity() + 1);
+        product.setQuantity(product.getQuantity() + product.getAmount());
     }
     }

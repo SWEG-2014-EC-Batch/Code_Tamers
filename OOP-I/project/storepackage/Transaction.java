8c1e589e3c496d6195c4d5e64ba567697ca75a8c
meron weldu <meron.w.teka@gmail.com>
2024-01-22 11:31:06 +0100

    Update Transaction.java

--- OOP-I/project/storepackage/Transaction.java
+++ OOP-I/project/storepackage/Transaction.java
@@ -3,29 +3,9 @@ package project.storepackage;
 // Transaction class
 public abstract class Transaction {
     // common attributes
-    private int id;
     private String date;
-    private double amount;
     
-    // constructor
-    public Transaction(int id, String date, double amount) {
-        this.id = id;
-        this.date = date;
-        this.amount = amount;
-    }
     
-    // getter methods
-    public int getId() {
-        return id;
-    }
-    
-    public String getDate() {
-        return date;
-    }
-    
-    public double getAmount() {
-        return amount;
-    }
     
     // abstract methods
     public abstract double calculate_price();

11e8cf0f7360726e5c126d31d3e46739a276bfd5
matiyasjosh <119722911+matiyasjosh@users.noreply.github.com>
2024-01-22 21:54:45 +0300

    Update Customer.java

--- OOP-I/project/user/Customer.java
+++ OOP-I/project/user/Customer.java
@@ -47,7 +47,22 @@ public class Customer extends User{
         cart.removeProduct(product);
     }
 
+    //see what the customer has added to the cart so far
     public void checkout() {
         cart.checkout();
+    }
+
+
+    //here is where the sales is done after the 
+    public void checkout(Inventory inventory) {
+        Sale sale = new Sale(this, cart); // Create a new Sale instance with the current customer and cart
+        double totalPrice = sale.calculate_price(); // Calculate the total price of the items in the cart
+        sale.payment(); // Process the payment from the customer
+        sale.update_stock(); // Update the stock of the products in the inventory
+    
+        inventory.record_transaction(sale); // Record the sale transaction in the inventory
+    
+        // Clear the cart after successful checkout
+        cart.getProducts().clear();
     }
 }

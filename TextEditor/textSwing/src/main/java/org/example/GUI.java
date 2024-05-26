70db23be38597aa0a221a2276be5343f2a80b224
Mastwal Mesfin <126337103+ENDragnee@users.noreply.github.com>
2024-05-26 11:05:12 +0300

    Added a functional redo and undo

--- TextEditor/textSwing/src/main/java/org/example/GUI.java
+++ TextEditor/textSwing/src/main/java/org/example/GUI.java
@@ -1,12 +1,14 @@
 package org.example;
 import javax.swing.*;
 import javax.swing.undo.UndoManager;
+import java.awt.*;
 import java.util.Vector;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class GUI extends JFrame implements ActionListener {
     JTextArea textArea;
+    Font defaultFont=new Font("Comic Sans MS",Font.PLAIN,12);
     boolean wWarp=false;
     JScrollPane scrollPane;
     JMenuBar menuBar;
@@ -21,21 +23,26 @@ public class GUI extends JFrame implements ActionListener {
     public GUI()
     {
         super("NotePad ");
+        super.setFont(defaultFont);
         textArea=new JTextArea();
         textArea.getDocument().addUndoableEditListener(
                 undoableEditEvent -> um.addEdit(undoableEditEvent.getEdit())
         );
+
         scrollPane=new JScrollPane(textArea,JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
         scrollPane.setBorder(BorderFactory.createEmptyBorder());
 
         addFont();
         addFontSize();
         menuBar=new JMenuBar();
+
         menuFile=new JMenu("File");
         menuEdit=new JMenu("Edit");
         menuFormat=new JMenu("Format");
         formatFont=new JMenu("Font");
+        formatFont.setFont(defaultFont);
         formatFontSize=new JMenu("Font size");
+        formatFontSize.setFont(defaultFont);
         createSubMenu();
 
         add(scrollPane);
@@ -43,43 +50,54 @@ public class GUI extends JFrame implements ActionListener {
         menuBar.add(menuFile);
         menuBar.add(menuEdit);
         menuBar.add(menuFormat);
-        format.selectedFont="Times";
-        format.makeFont(12);
+        format.selectedFont="Comics";
+        format.makeFont(16);
         format.wordWarp();
+
+        menuBar.setFont(defaultFont);
+        menuFile.setFont(defaultFont);
     }
 
     void createSubMenu(){
         iNew=new JMenuItem("New");
         iNew.addActionListener(this);
         iNew.setActionCommand("New");
+        iNew.setFont(defaultFont);
 
         iOpen=new JMenuItem("Open");
         iOpen.addActionListener(this);
         iOpen.setActionCommand("Open");
+        iOpen.setFont(defaultFont);
 
         iSave=new JMenuItem("Save");
         iSave.addActionListener(this);
         iSave.setActionCommand("Save");
+        iSave.setFont(defaultFont);
 
         iSaveAs=new JMenuItem("Save as");
         iSaveAs.addActionListener(this);
         iSaveAs.setActionCommand("SaveAs");
+        iSaveAs.setFont(defaultFont);
 
         iClose=new JMenuItem("Close");
         iClose.addActionListener(this);
         iClose.setActionCommand("Close");
+        iClose.setFont(defaultFont);
 
         iUndo=new JMenuItem("Undo");
         iUndo.addActionListener(this);
         iUndo.setActionCommand("Undo");
+        iUndo.setFont(defaultFont);
 
         iRedo=new JMenuItem("Redo");
         iRedo.addActionListener(this);
         iRedo.setActionCommand("Redo");
+        iRedo.setFont(defaultFont);
 
         iWordWarp=new JMenuItem("Word warp: Off");
         iWordWarp.addActionListener(this);
         iWordWarp.setActionCommand("WordWarp");
+        iWordWarp.setFont(defaultFont);
 
         menuFile.add(iNew);
         menuFile.add(iOpen);
@@ -96,6 +114,7 @@ public class GUI extends JFrame implements ActionListener {
         for(JMenuItem a:fonts)
         {
             formatFont.add(a);
+            a.setFont(defaultFont);
         }
         fonts.get(0).setActionCommand("Times");
         fonts.get(0).addActionListener(this);
@@ -108,6 +127,7 @@ public class GUI extends JFrame implements ActionListener {
         for(JMenuItem a:fontSize)
         {
             formatFontSize.add(a);
+            a.setFont(defaultFont);
         }
         fontSize.get(0).addActionListener(this);
         fontSize.get(0).setActionCommand("Size:8");

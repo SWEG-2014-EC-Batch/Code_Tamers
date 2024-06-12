d7763261d056201fd4dabf16e29fb895cfbb6ea2
me-ron <meronkiflayyy@gmail.com>
2024-06-12 12:08:46 +0300

    deleted not working files

--- Ip-II Projects/Udemy-clone/Backend/udemyclone/udemyclone/asgi.py
+++ Ip-II Projects/Udemy-clone/Backend/udemyclone/udemyclone/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for udemy_clone project.
+ASGI config for udemyclone project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'udemy_clone.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'udemyclone.settings')
 
 application = get_asgi_application()
